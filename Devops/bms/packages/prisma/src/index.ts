6131eb33a1b97f57ef4b157da819f6f2e4fd0463
simon <simonpaul496@gmail.com>
2026-07-19 18:39:31 -0500

    Completed the app

--- Devops/bms/packages/prisma/src/index.ts
+++ Devops/bms/packages/prisma/src/index.ts
@@ -1,3 +1,17 @@
-import { PrismaClient } from "../generated/prisma/client";
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/prisma/client.ts";
 
-export const client = new PrismaClient({} as any);
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not defined");
+}
+
+const adapter = new PrismaPg({
+  connectionString,
+});
+
+export const client = new PrismaClient({
+  adapter,
+});
